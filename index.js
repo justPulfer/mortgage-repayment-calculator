e4d918ece502cfa75d5f28d3ab6dc4b51998cddb
justPulfer <djlydia.pd@gmail.com>
2024-11-29 15:00:43 -0600

    added formulas and selectors for js

--- index.js
+++ index.js
@@ -7,3 +7,35 @@
 // - See hover and focus states for all interactive elements on the page
 
 "use strict";
+
+// Formulas:
+// Mortgage term in Months
+const morgageInMonths = mortgageTermInYears * 12;
+
+// How much interest is paid each month
+const monthlyInterestPayment = mortgageAmount * (annualInterestRate / 12);
+
+// How much interest is paid over the entire term
+const interestOverTerm = monthlyInterestPayment * morgageInMonths;
+
+// Monthly payment including interest
+const monthlyPaymentWithInterest =
+	mortgageAmount / morgageInMonths + monthlyInterestPayment;
+
+// Total repayment over the entire term including interest
+const repaymentOverTerm = monthlyPaymentWithInterest * morgageInMonths;
+
+// Dom Selectors
+const clearBtn = document.querySelector(".clear-btn");
+const mortgageAmountInput = document.querySelector("#mortgage-amount");
+const mortgageTermInput = document.querySelector("#mortgage-term");
+const interestRateInput = document.querySelector("#interest-rate");
+const typeRepaymentInput = document.querySelector("#repayment");
+const typeInterestOnlyInput = document.querySelector("#interest-only");
+const calculateBtn = document.querySelector(".calculate-btn");
+const monthlyRepaymentsDisplay = document.querySelector(
+	".monthly-repayments-display-value"
+);
+const totalRepaymentsDisplay = document.querySelector(
+	".total-repayments-display-value"
+);
